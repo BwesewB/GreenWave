0a89b19f4e298a9c494490b5c250a887421f5b1c
BwesewB <sebastianfok@gmail.com>
2024-04-15 11:32:50 -0700

    history changes

---
+++ .history/pages/index_20240406151501-ShWFreCyberpowerPC.js
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "@/styles/Home.module.css";
+import Earth from "@/components/Earth";
+import HeadArea from "@/components/HeadArea";
+import ButtonColoured from "@/components/mainButton";
+
+//Hi, so our idea requires earth to move dynaimcally around the screen, meaning that for this page we are only changing the elements and keeping the earth on one page for smoother animation of earth
+
+export default function Home() {
+  return (
+    <>
+      <HeadArea title="Welcome to Planit!" description="We are an environment app raising awareness on earths most pressing issues"/>
+      <main className={`${styles.main}`}>
+        <h1 className={styles.logoPlanit}>PlanIt</h1>
+        <ButtonColoured/>
+        <div>bebebeb</div>
+        <Earth/>
+      </main>
+    </>
+  );
+}
